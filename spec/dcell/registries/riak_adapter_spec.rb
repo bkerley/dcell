a1ea8124de01c921c87e9efcbbc7573c89d7d88a
Bryce Kerley <bkerley@brycekerley.net>
2012-11-26 21:35:38 -0500

    Add support for specifying RIAK_HTTP_PORT from an environment variable

--- spec/dcell/registries/riak_adapter_spec.rb
+++ spec/dcell/registries/riak_adapter_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe DCell::Registry::RiakAdapter do
-  subject { DCell::Registry::RiakAdapter.new :env => 'test' }
+  subject { DCell::Registry::RiakAdapter.new :env => 'test', http_port: ENV['RIAK_HTTP_PORT'] || 8098 }
   it_behaves_like "a DCell registry"
 end
